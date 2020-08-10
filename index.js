c373ab8ed4d6bd27bd61970dc87a1f7dfb9c8e32
xaviguasch <xaviguasch@gmail.com>
2020-08-10 19:24:49 +0200

    added js selectors

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('testing')
+// Create 4 variables and assign them to their respective elements: setupDiv, punchlinDiv, punchlineBtn, newJokeBtn
+
+const setupDiv = document.getElementById('setup')
+const punchlineDiv = document.getElementById('punchline')
+const punchlineBtn = document.getElementById('punchlineBtn')
+const newJokeBtn = document.getElementById('newJokeBtn')
